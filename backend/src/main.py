56a79b0d7d8fae5426766d6a814959fce86d8f81
van <vexyzy@yandex.ru>
2025-06-25 11:21:47 +0400

    fix: remove unused imports and decrease the lines to the <= 79 characters

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from uvicorn import run as server_start
 
 from api.routes import api_router
 from core.config import host_settings
 from core.logger.logger import get_configure_logger
-from db.dependencies.postgres_helper import postgres_helper
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url="/api/openapi.json",
+    docs_url="/api/openapi",
+    redoc_url="/api/redocapi",
+)
 
 app.include_router(api_router)
 
